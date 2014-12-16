3d13e24978d9ebc2daa69f829110ddec2074adff
Tobias Wooldridge <tobias@wooldridge.id.au>
2014-12-17 03:17:15 +1030

    Removes array literal from code

--- web/predictions.php
+++ web/predictions.php
@@ -6,7 +6,7 @@ require_once(__DIR__.'/../predict.php');
 $hyperBlip = $db->findBlipNearestTime(1, '2014-10-15 14:33:00'); //date('Y-m-d H:i:s.u', time()));
 
 if ($hyperBlip == null) {
-	echo json_encode(["error" => "Bus or tracker not active."]);
+	echo json_encode(array("error" => "Bus or tracker not active."));
 }
 
 $predictions = predictFutureStops($db, $hyperBlip[0]);
