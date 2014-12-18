3d760706b97b64d0a03325ab0495ecf8d9265a2c
Tobias Wooldridge <tobias@wooldridge.id.au>
2014-12-18 16:11:12 +1030

    Updated predictions error message for when no predictions shown

--- web/predictions.php
+++ web/predictions.php
@@ -3,7 +3,7 @@
 require_once(__DIR__.'/../db.php');
 require_once(__DIR__.'/../predict.php');
 
-$hyperBlip = $db->findBlipNearestTime(1, '2014-10-15 14:33:00'); //date('Y-m-d H:i:s.u', time()));
+$hyperBlip = $db->findBlipNearestTime(1, date('Y-m-d H:i:s.u', time()));
 
 if ($hyperBlip == null) {
 	echo json_encode(array("error" => "Bus or tracker not active."));
